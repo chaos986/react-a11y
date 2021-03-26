1e3ea1bd1bc74f0c398eafbd2d747341df56788a
Michael Honey-Arcement <chaos986@gmail.com>
2021-03-26 15:49:45 -0500

    practice using axe with react

--- chapter5/a11y/src/App.js
+++ chapter5/a11y/src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <a href="#home">Cool Link</a>
-        <img src="/img/is-an-image.jpg" alt="" />
-      </div >
+      <>
+        <header>
+          <h1>Hello World</h1>
+          <nav>
+            <a href="https://gwn-ws.com">My Web Site</a>
+          </nav>
+        </header>
+        <main>
+          <form role="search">
+            <input type="search" name="search" aria-label="Search all content" />
+          </form>
+        </main>
+      </>
     );
   }
 }
